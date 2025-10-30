866b3cbef55dc89999911accdc4d6a7327eb6f30
mike <jsdev.mas@gmail.com>
2025-10-30 18:35:23 +1100

    q3

---
+++ q3.js
@@ -0,0 +1,5 @@
+function increase() {
+  var counter = Number(document.getElementById("spanTagIncrease").innerHTML);
+  counter++;
+  document.getElementById("spanTagIncrease").innerHTML = counter;
+}
